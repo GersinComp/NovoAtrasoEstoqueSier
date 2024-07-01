508efb916f41036a489335dbde7f9c1c41406d6c
gersin <gersonfernandesribeiro48@gmail.com>
2024-06-30 23:55:25 -0300

    commit __init__
    Correção do problema com o usuário

--- PackArquivos/__init__.py
+++ PackArquivos/__init__.py
@@ -2,25 +2,20 @@ import secrets
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from dotenv import load_dotenv
 
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
 
 load_dotenv()
 
 tokenForms = secrets.token_hex(16)
 app.config['SECRET_KEY'] = tokenForms
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CONECTOR')
+if os.getenv('CONECTOR'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CONECTOR')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///atraso.db'
 
 
 db = SQLAlchemy(app)
-login_manager.login_view = 'login'
-login_manager.login_message = 'Por favor, faça login para acessar essa página.'
-login_manager.login_message_category = 'alert-info'
-
 from PackArquivos import routes
--- PackArquivos/models.py
+++ PackArquivos/models.py
@@ -1,13 +1,7 @@
 import secrets
 from datetime import datetime
 from pytz import timezone
-from PackArquivos import app, db, login_manager
-from flask_login import UserMixin
-
-
-@login_manager.user_loader
-def load_usuario(id_usuario):
-    return Usuario.query.get(id_usuario)
+from PackArquivos import app, db
 
 
 def horarioCriado():
@@ -16,30 +10,6 @@ def horarioCriado():
 
     # Obtém a data e hora atual em Brasília
     return datetime.now(brasilia_timezone).strftime('%Y/%m/%d')
-
-
-class Usuario(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(45), nullable=False)
-    sobrenome = db.Column(db.String(80), nullable=False)
-    nascimento = db.Column(db.Date, nullable=False, default="0001-01-01")
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    telefone = db.Column(db.String(11), nullable=False)
-    senha = db.Column(db.String(80), nullable=False)
-    foto_perfil = db.Column(db.String(80), default='default.jpg')
-    recibo = db.relationship('Recibo', backref='usuario', lazy=True)
-
-
-class Recibo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    empresa = db.Column(db.String(80), nullable=False)
-    valor = db.Column(db.String(80), nullable=False)
-    data = db.Column(db.Date, nullable=False, default=horarioCriado)
-    nome = db.Column(db.String(80), nullable=False)
-    cpf = db.Column(db.String(11), nullable=False)
-    descricao = db.Column(db.Text(), nullable=False)
-    token = db.Column(db.String(8), nullable=False, default=secrets.token_hex(4))
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
 
 class Cadeiras(db.Model):
